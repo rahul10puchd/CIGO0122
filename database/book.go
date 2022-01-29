4343ba06a708fd51a11e57ccc864ead4d54f1d09
Rahul Kumar <rahul10.pu@gmail.com>
2022-01-29 09:49:39 +0530

    models, database, gin

---
+++ database/book.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/rahul10-pu/CIGO0122/models"
+)
+
+func GetBooks(db *gorm.DB) ([]models.Book, error) {
+
+	books := []models.Book{}
+	query := db.Select("books.*")
+	err := query.Find(&books).Error
+	if err != nil {
+		return nil, err
+	}
+	return books, nil
+}
+func GetBookByID(db *gorm.DB, id string) (models.Book, error) {
+	return models.Book{}, nil
+}
+func DeleteBookByID(db *gorm.DB, id string) error {
+	return nil
+}
+func UpdateBookByID(db *gorm.DB, book *models.Book) error {
+	return nil
+}
