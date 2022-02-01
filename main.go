6591edb7b046462410d7bf8618bb854a561ab752
Rahul Kumar <rahul10.pu@gmail.com>
2022-02-01 08:47:20 +0530

    init

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/rahul10-pu/CIGO0122/database"
 	"github.com/rahul10-pu/CIGO0122/routers"
 )
 
+func init() { //1st
+	database.Setup() //establish the database connection
+}
+func init() { //2nd
+	fmt.Print(1)
+}
+func init() { //3nd
+	fmt.Print(2)
+}
 func main() {
-	database.Setup()                    //establish the database connection
+
 	engine := routers.Router()          //get the customized engine
 	err := engine.Run("127.0.0.1:8080") //start the engine
 	if err != nil {
