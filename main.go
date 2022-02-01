d8992a902fc26a656c2168869dd57b29b59855ed
Rahul Kumar <rahul10.pu@gmail.com>
2022-02-01 09:36:53 +0530

    map

--- main.go
+++ main.go
@@ -1,49 +1,65 @@
 package main
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"os"
-)
+import "fmt"
 
-type Something struct {
-	Name    string    `json:"name"`
-	Married bool      `json:"married"`
-	Age     float64   `json:"age"`
-	Address []Address `json:"address"`
-	Marks   []int     `json:"marks"`
-}
-type Address struct {
-	Street int    `json:"street"`
-	City   string `json:"city"`
-}
+// type Something struct {
+// 	Name    string    `json:"name"`
+// 	Married bool      `json:"married"`
+// 	Age     float64   `json:"age"`
+// 	Address []Address `json:"address"`
+// 	Marks   []int     `json:"marks"`
+// }
+// type Address struct {
+// 	Street int    `json:"street"`
+// 	City   string `json:"city"`
+// }
 
+// func main() {
+// 	jsonFile, err := os.Open("something.json")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer jsonFile.Close()
+// 	jsonByteValues, err := ioutil.ReadAll(jsonFile)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	var something Something
+// 	json.Unmarshal(jsonByteValues, &something) //converting json data to struct
+// 	log.Println(something)
+// 	// fmt.Print(string(jsonByteValues)) //converting json data to string
+// 	// newJsonByteValues, err := json.Marshal(something)
+// 	// if err != nil {
+// 	// 	log.Fatal(err)
+// 	// }
+// 	// os.WriteFile("some.json",newJsonByteValues)
+
+// 	//consuming rest api in go code
+// 	//using the rest api
+// 	//returned value ? = json
+// 	//now you must know how to read the json data
+// 	// what is the problem if you read json data in string format
+// 	// read json in strcture format - marshalling , unmarshalling
+
+// }
 func main() {
-	jsonFile, err := os.Open("something.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer jsonFile.Close()
-	jsonByteValues, err := ioutil.ReadAll(jsonFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var something Something
-	json.Unmarshal(jsonByteValues, &something) //converting json data to struct
-	log.Println(something)
-	// fmt.Print(string(jsonByteValues)) //converting json data to string
-	// newJsonByteValues, err := json.Marshal(something)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// os.WriteFile("some.json",newJsonByteValues)
+	//var user map[int]string //user just got declared, no memory assigned
+	user := map[int]interface{}{}
+	user[1] = "name"
+	user[2] = true
+	// fmt.Println(user[2])
+	//set
+	//does not have duplicate value
+	//check or parse in the set
+	//use set feature via map
+	users := map[string]bool{}
+	users["golang"] = true
 
-	//consuming rest api in go code
-	//using the rest api
-	//returned value ? = json
-	//now you must know how to read the json data
-	// what is the problem if you read json data in string format
-	// read json in strcture format - marshalling , unmarshalling
+	// value, ok := users["rahul"]
+	// if ok == false {
+	// 	fmt.Println("value not there")
+	// }
+
+	fmt.Println(users["rahul"])
 
 }
