a835934daa92633ad775708601d9d435ac6fc0fd
Rahul Kumar <rahul10.pu@gmail.com>
2022-02-02 08:23:54 +0530

    added delete and put method request

--- handler/book.go
+++ handler/book.go
@@ -36,6 +36,38 @@ func (h *Handler) SaveBook(in *gin.Context) {
 	}
 	in.JSON(http.StatusOK, book)
 }
+func (h *Handler) GetBookByID(in *gin.Context) {
+	id := in.Params.ByName("id")
+	book, err := database.GetBookByID(h.DB, id) //h.DB is fully configured and can give the access of book table
+	if err != nil {
+		log.Println(err)
+		in.JSON(http.StatusInternalServerError, err)
+	}
+	in.JSON(http.StatusOK, book)
+}
+func (h *Handler) DeleteBookByID(in *gin.Context) {
+	id := in.Params.ByName("id")
+	err := database.DeleteBookByID(h.DB, id) //h.DB is fully configured and can give the access of book table
+	if err != nil {
+		log.Println(err)
+		in.JSON(http.StatusInternalServerError, err)
+	}
+	in.JSON(http.StatusOK, id)
+}
+func (h *Handler) UpdateBookByID(in *gin.Context) {
+	book := models.Book{}
+	err := in.BindJSON(&book)
+	if err != nil {
+		log.Println(err)
+		in.JSON(http.StatusInternalServerError, err)
+	}
+	err = database.UpdateBookByID(h.DB, &book) //h.DB is fully configured and can give the access of book table
+	if err != nil {
+		log.Println(err)
+		in.JSON(http.StatusInternalServerError, err)
+	}
+	in.JSON(http.StatusOK, book)
+}
 
 /*
 h:=Handler{}
--- routers/book.go
+++ routers/book.go
@@ -15,6 +15,9 @@ func Router() *gin.Engine {
 	router.GET("/books", api.GetBooks) //set the function for http://localhost:8080/books : Get request
 	//while calling handler function, gin will pass *gin.Context in the handler function
 	router.POST("/books", api.SaveBook)
+	router.GET("/books/:id", api.GetBookByID)
+	router.DELETE("/books/:id", api.DeleteBookByID)
+	router.PUT("/books", api.UpdateBookByID)
 	//	router.DELETE("/books/:id", api.DeleteBook)
 	return router
 }
